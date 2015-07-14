12bdd54cbe23a74bc9971875fd4b5ddc41507f36
alu20alu20 <student10@js-c02mxf7ug083.punahou.edu>
2015-07-14 11:26:56 -1000

    this is new version

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
-var computerChoice;
-function computerThink(){
-	computerChoice = Math.floor(Math.random()*100);
-	if(computerChoice < 5){
-		computerChoice = Diamond;
-		diamondPoint++;
-			alert("You Got Diamond");
-}
+var score = 0;
 
+var silverPoint = 10;
 
-	else if(computerChoice > 31){
-		computerChoice = Nothing;
-		nothingPoint++;	
-			alert("You Got Nothing");	
-}
-	else if(computerChoice > 5 && computerChoice < 16){
-		computerChoice = Gold;
-		goldPoint++;
-			alert("You Got Gold");	
-}
-	else if(computerChoice > 15 && computerChoice < 31){
-		computerChoice = Silver;
-		silverPoint++;
-			alert("You Got Silver");	
-}
-}
-
-Score = diamondPoint*50 + silverPoint*10 + goldPoint*25 + nothingPoint*1;
-
-var diamondPoint = 1;
-
-var silverPoint = 1;
-
-var goldPoint = 1;
+var goldPoint = 25;
 
 var nothingPoint = 1;
 
-var Score = 0;
+var diamondPoint = 50;
 
-var press = function(){
-	score++;
-	document.getElementById('score').innerHTML = "Your Score: " + score;
-};
+var computerChoice;
+
+var start = new Date();
+
+setInterval(function() {
+    $('#Timer').text((new Date() - start) / 1000 + " Seconds");
+}, 1000);
+	//Trying to change the timer to no decimal places//
+
+$("#zane").click(function(){
+	computerChoice = Math.floor(Math.random()*100);
+	if(computerChoice < 5){
+		computerChoice = "Diamond";
+			$('#received').text("You got Diamond");
+			score = diamondPoint + score;
+			$('#Score').text('Your Score ' + score);
+	}
+	else if(computerChoice > 31){
+		computerChoice = "Nothing";	
+			$('#received').text("You got Rocks");
+			score = nothingPoint + score;	
+			$('#Score').text('Your Score ' + score);
+
+	}	
+	else if(computerChoice > 5 && computerChoice < 16){
+		computerChoice = "Gold";
+			$('#received').text("You got Gold");
+			score = goldPoint + score;
+			$('#Score').text('Your Score ' + score);
+	}
+	else if(computerChoice > 15 && computerChoice < 31){
+		computerChoice = "Silver";
+			$('#received').text("You got Silver");	
+			score = silverPoint + score;
+			$('#Score').text('Your Score ' + score);
+	}
+});
+
+
+
+
+
 
 
 
